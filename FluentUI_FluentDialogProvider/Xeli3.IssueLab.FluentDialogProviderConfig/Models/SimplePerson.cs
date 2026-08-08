fe43437aec95ba8f6ed54a428096099808ff05c5
xaberue <xavier.aberue@outlook.es>
2026-08-08 19:34:14 +0200

    Server componet works

---
+++ FluentUI_FluentDialogProvider/Xeli3.IssueLab.FluentDialogProviderConfig/Models/SimplePerson.cs
@@ -0,0 +1,8 @@
+﻿namespace Xeli3.IssueLab.FluentDialogProviderConfig.Models;
+
+public class SimplePerson
+{
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public int Age { get; set; }
+}
