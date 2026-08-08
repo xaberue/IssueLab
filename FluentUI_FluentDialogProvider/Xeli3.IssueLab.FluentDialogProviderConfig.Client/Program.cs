632cee83f048638de4602b134dfe24c355806892
xaberue <xavier.aberue@outlook.es>
2026-08-08 19:22:59 +0200

    Template setup

---
+++ FluentUI_FluentDialogProvider/Xeli3.IssueLab.FluentDialogProviderConfig.Client/Program.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+
+builder.Services.AddFluentUIComponents();
+
+await builder.Build().RunAsync();
---
+++ FluentUI_FluentDialogProvider/Xeli3.IssueLab.FluentDialogProviderConfig/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.FluentUI.AspNetCore.Components;
+using Xeli3.IssueLab.FluentDialogProviderConfig.Client.Pages;
+using Xeli3.IssueLab.FluentDialogProviderConfig.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddFluentUIComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Xeli3.IssueLab.FluentDialogProviderConfig.Client._Imports).Assembly);
+
+app.Run();
